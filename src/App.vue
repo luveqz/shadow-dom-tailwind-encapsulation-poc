7cc562079f7c51a68a77d155096c6b3a1e4fc6dc
Luis Velásquez <luvejo.dev@gmail.com>
2024-07-03 20:29:48 -0500

    chore: add modal window

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const showModal = ref(false);
+</script>
+
 <template>
   <div class="bg-primary">
     <p class="underline">Hello, Friend.</p>
+
+    <button @click="showModal = true">Show Modal</button>
+
+    <div
+      v-if="showModal"
+      class="bg-black/45 absolute left-0 top-0 w-full h-full flex justify-center items-center"
+    >
+      <div class="modal-content">
+        <span
+          @click="showModal = false"
+          class="bg-white p-2 m-10 flex items-end justify-center rounded-md cursor-pointer absolute top-0 right-0"
+          >Close</span
+        >
+        <p>Modal Content</p>
+      </div>
+    </div>
   </div>
 </template>
